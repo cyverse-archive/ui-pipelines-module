256627acec0dcc6e4d3409a6e308baa5e05f667c
Paul Sarando <psarando@iplantcollaborative.org>
2013-03-01 15:23:14 -0700

    Updated the AppsViewPresenter go method used to load groups.
    Currently, the go(HasOneWidget, HasId, HasId) method is used to load App groups from the proxy.
    Updated PipelineViewPresenter to use this method (via the builder) in its initAppsView method to load the groups.

--- src/main/java/org/iplantc/core/client/pipelines/gxt3/presenter/PipelineViewPresenter.java
+++ src/main/java/org/iplantc/core/client/pipelines/gxt3/presenter/PipelineViewPresenter.java
@@ -93,7 +93,7 @@ public class PipelineViewPresenter implements Presenter, PipelineView.Presenter,
                 .hideToolbarButtonEdit()
                 .hideToolbarButtonRequestTool()
                 .hideToolbarButtonSubmit()
-                .go(appSelectView);
+                .go(appSelectView, null, null);
     }
 
     private void initAppsGridDragHandler(Grid<App> grid) {
