3b0baaa003f44bfc7b8c4254e46f2f0cafb11b6e
Jonathan Strootman <jstroot@iplantcollaborative.org>
2014-02-03 16:22:26 -0700

    Performed package re-org of ui-pipelines-module

---
+++ src/main/java/org/iplantc/de/pipelines/client/models/ServicePipelineStepConfig.java
@@ -0,0 +1,11 @@
+package org.iplantc.de.pipelines.client.models;
+
+/**
+ * An AutoBean interface for a service Pipeline step config.
+ * 
+ * @author psarando
+ * 
+ */
+public interface ServicePipelineStepConfig {
+
+}
